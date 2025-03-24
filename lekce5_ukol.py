c53baecbddd08911d87253ee8a262f8ab1e7031c
JirkaR <jarka747@gmail.com>
2025-03-24 18:09:24 +0100

    úkol doplněn o dobrovolnou část

--- lekce5_ukol.py
+++ lekce5_ukol.py
@@ -1,6 +1,9 @@
+from enum import nonmember
+
 numbers = [1, 2, 4, -6, 7, 8, 100, -125, 11, 123]
-names = ["Petr", "Ales", "Honza", "Lenka", "Andrea","Alice"]
-random_codes = ["1-okdsaaa", "0-nFnldd", "0-AA", "0-uwqqq", "2-ZSTh", "0-RKOcsxxx", "1-LwWtss", "0-cdKiddd", "2-KpAAaa", "3-sOdSxhcds"]
+names = ["Petr", "Ales", "Honza", "Lenka", "Andrea", "Alice"]
+random_codes = ["1-okdsaaa", "0-nFnldd", "0-AA", "0-uwqqq", "2-ZSTh", "0-RKOcsxxx", "1-LwWtss", "0-cdKiddd", "2-KpAAaa",
+                "3-sOdSxhcds"]
 
 # 1. pomocí cyklu for vypište čísla ze seznamu numbers, ale přeskočte záporná čísla.
 
@@ -11,27 +14,43 @@ random_codes = ["1-okdsaaa", "0-nFnldd", "0-AA", "0-uwqqq", "2-ZSTh", "0-RKOcsxx
 # Dobrovolny ukol pro pokrocile (nebude bodovan): Vypis všechny pod-seznamy s alespoň třemi po sobě jdoucími stejnými znaky v seznamu 'random_codes'
 
 
-
 # úkol 1 - v cyklu se vypíší jen kladná čísla
 
 print("úkol číslo 1 : ")
 for number in numbers:
-    if number >0:
+    if number > 0:
         print(number)
 
 # úkol 2 - cyklus vypíše všechna jména a skončí pokud narazí na Alice
 print("úkol číslo 2 : ")
-i=0
-while len(names)>i:
+i = 0
+while len(names) > i:
     if names[i] == "Alice":
         break
     print(names[i])
-    i+=1
+    i += 1
 
 # úkol 3 - cyklus vybere jen čísla která obsahují dvě nuly
 
-zero_list= [code for code in random_codes if "0" in code]
+zero_list = [code for code in random_codes if "0" in code]
 
 print(zero_list)
 
+# Dobrovolný úkol
+print("Dobrovolný úkol")
+word = None
+k = 0
+while len(random_codes) > k:
+    word = random_codes[k]
 
+    j = 0
+    while len(word) > j:
+        char = word[j]
+        if len(word) - j < 3:
+            break
+        if char == word[j + 1] and char == word[j + 2]:
+            print(word)
+            break
+
+        j += 1
+    k += 1

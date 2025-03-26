4328ddc2d63a05145ad8c433bfc90eb815468caf
JirkaR <jarka747@gmail.com>
2025-03-26 13:09:42 +0100

    rozpracování úkolu č.7

---
+++ lekce7_ukol.py
@@ -0,0 +1,62 @@
+# Napiš program, který bude pracovat s informacemi o uživatelském účtu: username, age, email
+#
+#
+# Vytvoř následující funkce:
+#
+# is_adult: Funkce ověří zda je uživatel dospělý
+# is_name_valid: Funkce ověří zda uživatelské jméno je alespoň 4 znaky dlouhé
+# create_user:
+#   Funkce vytvoří slovník reprezentujícího uživatele.
+#  Uvnitř funkce zkontroluj, zda je uživatel dospělý a jeho jméno je validní.
+# Pokud je vše v pořádku, create_user vrátí následující slovník:
+# {
+# "success": True,
+# "user": {"username": "...", "age": ..., "email": "..."}
+# }
+#  Pokud validace selže, create_user vrátí:
+# {
+# "success": False,
+# "error": "Chybová zpráva..."
+# }
+#
+#
+# print_user_info: Funkce vytiskne uživatele do konzole s libovolným formátováním, případně vytiskne chybovou zprávu při neúspěšném vytvoření
+#
+# Pomocí metody create_user vytvoř alespoň 4 různé uživatele. Hodnoty si zvol podle sebe přímo v programu.
+#
+# Nakonec vytvořené uživatele vytiskni pomocí cyklu a metody print_user_info.
+
+
+# funkce ověčí zda je uživatel dospělý
+def is_adult(age: int) -> bool:
+    if age > 18:
+        return True
+    else:
+        return False
+
+
+# funkce ověří zda jde o platné jméno
+def is_name_valid(user_name: str) -> bool:
+    if len(user_name) >= 4:
+        return True
+    else:
+        return False
+
+
+# funkce pro vytvoření slovníku reprezentující uživatele
+
+def create_user(username: str, age: int, email: str) -> {}:
+    if is_adult(age) == False:
+        print("chyba věk")
+    if is_name_valid(username) == False:
+        print("chyba jméno")
+
+    return {"username":username,"age":age,"email":email}
+
+username=input("jmeno: ")
+age=input("vek: ")
+age=int(age)
+email=input("email: ")
+
+user=create_user(username,age,email)
+print(user)
